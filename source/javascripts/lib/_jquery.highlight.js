db0ec269fd1979e84071c94e9365c03477759e96
Darius <313kid@gmail.com>
2018-06-13 14:39:00 -0700

    license info

--- source/javascripts/lib/_jquery.highlight.js
+++ source/javascripts/lib/_jquery.highlight.js
@@ -1,4 +1,13 @@
 /*
+
+Copyright (c) 2009 Bartek Szopka, Johann Burkard <http://johannburkard.de>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the ""Software""), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
  * jQuery Highlight plugin
  *
  * Based on highlight v3 by Johann Burkard
@@ -36,10 +45,6 @@
  *   // remove custom highlight
  *   $('#content').unhighlight({ element: 'em', className: 'important' });
  *
- *
- * Copyright (c) 2009 Bartek Szopka
- *
- * Licensed under MIT license.
  *
  */
 
@@ -82,7 +87,7 @@ jQuery.fn.unhighlight = function (options) {
 jQuery.fn.highlight = function (words, options) {
     var settings = { className: 'highlight', element: 'span', caseSensitive: false, wordsOnly: false };
     jQuery.extend(settings, options);
-    
+
     if (words.constructor === String) {
         words = [words];
     }
@@ -100,9 +105,8 @@ jQuery.fn.highlight = function (words, options) {
         pattern = "\\b" + pattern + "\\b";
     }
     var re = new RegExp(pattern, flag);
-    
+
     return this.each(function () {
         jQuery.highlight(this, re, settings.element, settings.className);
     });
 };
-
